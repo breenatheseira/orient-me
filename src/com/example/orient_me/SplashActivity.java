770646b3f1a598d16986628262682e99dd57a6c4
Breena Theseira <breenatheseira@yahoo.com>
2015-04-05 00:37:17 +0800

    Updated: Added code for a simple splash screen of 3 minutes.

--- src/com/example/orient_me/SplashActivity.java
+++ src/com/example/orient_me/SplashActivity.java
@@ -1,5 +1,27 @@
 package com.example.orient_me;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+
+public class SplashActivity extends ActionBarActivity {
+	private static int splash_timeout = 3000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent intent = new Intent(getApplicationContext(),
+						LoginActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		}, splash_timeout);
+	}
 
 }
