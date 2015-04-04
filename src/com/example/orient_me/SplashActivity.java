5be23abda2270be08b373ae13ac4f2ca4409627f
Breena Theseira <breenatheseira@yahoo.com>
2015-04-05 00:55:53 +0800

    Updated: Splash screen will direct student to Login Activity if student has not logged in yet; else if the student has logged in (presence of auth_token), student will be redirected to Main menu screen.

--- src/com/example/orient_me/SplashActivity.java
+++ src/com/example/orient_me/SplashActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
 
+// Tamada, R. (2013): http://www.androidhive.info/2013/07/how-to-implement-android-splash-screen-2/
 public class SplashActivity extends ActionBarActivity {
 	private static int splash_timeout = 3000;
 
@@ -15,10 +17,16 @@ public class SplashActivity extends ActionBarActivity {
 
 		new Handler().postDelayed(new Runnable() {
 			@Override
-			public void run() {
-				Intent intent = new Intent(getApplicationContext(),
-						LoginActivity.class);
-				startActivity(intent);
+			public void run() {			
+				PreferencesHelper pref = new PreferencesHelper(getApplicationContext());
+				
+				Intent intent_login = new Intent(getApplicationContext(),LoginActivity.class);
+				Intent intent_menu = new Intent(getApplicationContext(), MainActivity.class);
+				
+				if (pref.GetPreferences("AuthToken").length() == 0)
+					startActivity(intent_login);
+				else
+					startActivity(intent_menu);
 				finish();
 			}
 		}, splash_timeout);
