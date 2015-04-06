39069d0354a8572cb244a81f1aea0d0e62f42b6a
Breena Theseira <breenatheseira@yahoo.com>
2015-04-06 17:04:18 +0800

    Updated: Button ids are more meaningful in document activity, and variables were declared in the DocumentActivity class for future use.

--- src/com/example/orient_me/DocumentActivity.java
+++ src/com/example/orient_me/DocumentActivity.java
@@ -2,20 +2,26 @@ package com.example.orient_me;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class DocumentActivity extends ActionBarActivity{
-
+public class DocumentActivity extends ActionBarActivity{	
+	
+	Button stuHandbook, modList, campMap, orientSch, myNotes;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_document);
 		Toast.makeText(DocumentActivity.this, "Welcome, student!", Toast.LENGTH_SHORT).show();
+		
+		stuHandbook = (Button) findViewById(R.id.daB_studentHandbook);
+		modList = (Button) findViewById(R.id.daB_moduleList);
+		campMap = (Button) findViewById(R.id.daB_campusMap);
+		orientSch= (Button) findViewById(R.id.daB_orientSchedule);
+		myNotes = (Button) findViewById(R.id.daB_myNotes);
 	}
 
 	@Override
