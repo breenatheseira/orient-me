f5234690a048bec3ab9dd492be22217b0d1395b8
Breena Theseira <breenatheseira@yahoo.com>
2015-04-06 10:27:41 +0800

    Updated: Menu set in R.menu.main is shown in the top left corner of the application on Android runtime.

--- src/com/example/orient_me/DocumentActivity.java
+++ src/com/example/orient_me/DocumentActivity.java
@@ -2,6 +2,11 @@ package com.example.orient_me;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Toast;
 
 public class DocumentActivity extends ActionBarActivity{
@@ -12,5 +17,13 @@ public class DocumentActivity extends ActionBarActivity{
 		setContentView(R.layout.activity_document);
 		Toast.makeText(DocumentActivity.this, "Welcome, student!", Toast.LENGTH_SHORT).show();
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);	
+		return super.onCreateOptionsMenu(menu);
+	}
+
 
 }
