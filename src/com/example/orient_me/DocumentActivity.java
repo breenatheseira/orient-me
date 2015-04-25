a1f4971885ca0886420483e65840367c34f2133e
Breena Theseira <breenatheseira@yahoo.com>
2015-04-25 21:53:42 +0800

    Updated: PDFs that are downloaded could be viewed in DocumentActivity

--- src/com/example/orient_me/DocumentActivity.java
+++ src/com/example/orient_me/DocumentActivity.java
@@ -1,6 +1,12 @@
 package com.example.orient_me;
 
+import java.io.File;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,14 +15,13 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class DocumentActivity extends ActionBarActivity implements OnClickListener {	
-	
-	Button stuHandbook, modList, campMap, orientSch, myNotes;
-	
+public class DocumentActivity extends ActionBarActivity implements OnClickListener {		
+	Button stuHandbook, modList, campMap, orientSch, myNotes;	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_document);
+		
 		Toast.makeText(DocumentActivity.this, "Welcome, student!", Toast.LENGTH_SHORT).show();
 		
 		stuHandbook = (Button) findViewById(R.id.daB_studentHandbook);
@@ -47,24 +52,39 @@ public class DocumentActivity extends ActionBarActivity implements OnClickListen
 
 	@Override
 	public void onClick(View v) {
+		PreferencesHelper prefs = new PreferencesHelper(getApplicationContext());
 		switch (v.getId()){
 		case R.id.daB_studentHandbook:
-			
+			ViewPDF(prefs.GetPreferences("StudentHandbook"),v);
 			break;
 		case R.id.daB_moduleList:
-			
+			ViewPDF(prefs.GetPreferences("ModuleList"),v);
 			break;
 		case R.id.daB_campusMap:
-			
+			ViewPDF(prefs.GetPreferences("StudentHandbook"),v);
 			break;
 		case R.id.daB_orientSchedule:
-			
+			ViewPDF(prefs.GetPreferences("OrientationSchedule"),v);
 			break;
 		case R.id.daB_myNotes:
-			
+			ViewPDF(prefs.GetPreferences("StudentHandbook"),v);
 			break;
 		}
 	}
 
+    public void ViewPDF(String filepath, View v)
+    {
+        File pdfFile = new File(filepath);
+        Uri path = Uri.fromFile(pdfFile);
+        Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+        pdfIntent.setDataAndType(path, "application/pdf");
+        pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        try{
+            startActivity(pdfIntent);
+        }catch(ActivityNotFoundException e){
+            Toast.makeText(DocumentActivity.this, "No Application available to view PDF", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
