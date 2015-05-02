95b8e5c1b142d25a62784e47398825114a32fb72
Breena Theseira <breenatheseira@yahoo.com>
2015-05-02 21:00:41 +0800

    Added: Note class and model package

---
+++ src/com/example/orient_me/models/Note.java
@@ -0,0 +1,37 @@
+package com.example.orient_me.models;
+
+public class Note {
+String id, title, note;
+	
+	public Note(){}
+	
+	public Note(String id, String title, String note){
+		this.id = id;
+		this.title = title;
+		this.note = note;
+	}
+	
+	public void setId(String id){
+		this.id = id;
+	}
+	
+	public void setTitle(String title){
+		this.title = title;
+	}
+	
+	public void setNote(String note){
+		this.note = note;
+	}
+	
+	public String getId(){
+		return this.id;
+	}
+	
+	public String getDocId(){
+		return this.title;
+	}
+	
+	public String getContent(){
+		return this.note;
+	}
+}
