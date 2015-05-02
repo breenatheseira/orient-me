56414a17b1513028a1f63e87f6fc4fffad6e7722
Breena Theseira <breenatheseira@yahoo.com>
2015-05-02 21:07:54 +0800

    Added: DatabaseHelper class to define database

---
+++ src/com/example/orient_me/helpers/DatabaseHelper.java
@@ -0,0 +1,54 @@
+package com.example.orient_me.helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+	// Database Version
+		protected static final int DATABASE_VERSION = 1;
+
+		// Database Name
+		protected static final String DATABASE_NAME = "orientMe";
+		
+		// Table Names
+		private static final String TABLE_NOTES = "notes";
+		
+		
+		// Notes Column name
+		private static final String KEY_NOTE_ID = "id";
+		private static final String KEY_TITLE = "title";
+		private static final String KEY_NOTE = "note";
+
+		
+		// Table Create Statements
+			// Note table create statement
+			private static final String CREATE_TABLE_NOTES = "CREATE TABLE "
+					+ TABLE_NOTES + "(" + KEY_NOTE_ID + " INTEGER PRIMARY KEY,"
+					+ KEY_TITLE + " TEXT," + KEY_NOTE + " TEXT" + ")";
+			
+		public DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// creating required tables
+			db.execSQL(CREATE_TABLE_NOTES);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// on upgrade drop older tables
+			db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTES);
+
+			// create new tables
+			onCreate(db);
+		}
+
+		public void closeDB() {
+			SQLiteDatabase db = this.getReadableDatabase();
+			if (db != null && db.isOpen())
+				db.close();
+		}
+}
