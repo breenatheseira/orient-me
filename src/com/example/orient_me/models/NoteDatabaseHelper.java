19c4c2793592e1d5b8e9cd51d21ce9a8fb77300d
Breena Theseira <breenatheseira@yahoo.com>
2015-05-02 21:09:53 +0800

    Added: NoteDatabaseHelper class for all Notes table database methods

---
+++ src/com/example/orient_me/models/NoteDatabaseHelper.java
@@ -0,0 +1,13 @@
+package com.example.orient_me.models;
+
+import android.content.Context;
+
+import com.example.orient_me.helpers.DatabaseHelper;
+
+public class NoteDatabaseHelper extends DatabaseHelper {
+
+	public NoteDatabaseHelper(Context context) {
+		super(context);
+	}
+
+}
