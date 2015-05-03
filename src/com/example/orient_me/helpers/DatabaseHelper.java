af13d4dcb201233a19c205cf7325cbb910122166
Breena Theseira <breenatheseira@yahoo.com>
2015-05-03 09:47:05 +0800

    Updated: Added DLL to create the structure of Schedules table

--- src/com/example/orient_me/helpers/DatabaseHelper.java
+++ src/com/example/orient_me/helpers/DatabaseHelper.java
@@ -13,19 +13,40 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		
 		// Table Names
 		protected static final String TABLE_NOTES = "notes";
+		protected static final String TABLE_SCHEDULES = "schedules";
 		
-		
-		// Notes Column name
+		// Notes Column names
 		protected static final String KEY_NOTE_ID = "id";
 		protected static final String KEY_TITLE = "title";
 		protected static final String KEY_NOTE = "note";
 
+		// Schedule Column names
+		protected static final String KEY_SCHEDULE_ID = "id";
+		protected static final String KEY_SCHEDULE_TITLE = "title";
+		protected static final String KEY_SCHEDULE_NOTES = "note";
+		protected static final String KEY_SCHEDULE_START = "startTime";
+		protected static final String KEY_SCHEDULE_END = "endTime";
+		protected static final String KEY_SCHEDULE_LOCATION = "location";
+		protected static final String KEY_SCHEDULE_ALERT = "alert";
+		protected static final String KEY_SCHEDULE_TRANSPARENT = "transparent";
 		
 		// Table Create Statements
 			// Note table create statement
 			private static final String CREATE_TABLE_NOTES = "CREATE TABLE "
 					+ TABLE_NOTES + "(" + KEY_NOTE_ID + " INTEGER PRIMARY KEY,"
 					+ KEY_TITLE + " TEXT," + KEY_NOTE + " TEXT" + ")";
+			
+			// Schedule table create statement
+			private static final String CREATE_TABLE_SCHEDULES = "CREATE TABLE "
+					+ TABLE_SCHEDULES + "("
+					+ KEY_SCHEDULE_ID + " INTEGER PRIMARY KEY,"
+					+ KEY_SCHEDULE_TITLE + " TEXT,"
+					+ KEY_SCHEDULE_START + " DATETIME,"
+					+ KEY_SCHEDULE_END + " DATETIME,"
+					+ KEY_SCHEDULE_LOCATION + " TEXT,"
+					+ KEY_SCHEDULE_ALERT + " INTEGER,"
+					+ KEY_SCHEDULE_NOTES + " TEXT,"
+					+ KEY_SCHEDULE_TRANSPARENT + " INTEGER" + ")";
 			
 		public DatabaseHelper(Context context) {
 			super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -35,12 +56,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		public void onCreate(SQLiteDatabase db) {
 			// creating required tables
 			db.execSQL(CREATE_TABLE_NOTES);
+			db.execSQL(CREATE_TABLE_SCHEDULES);
 		}
 
 		@Override
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 			// on upgrade drop older tables
 			db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTES);
+			db.execSQL("DROP TABLE IF EXISTS " + TABLE_SCHEDULES);
 
 			// create new tables
 			onCreate(db);
--- src/com/example/orient_me/schedules/ScheduleDatabaseHelper.java
+++ src/com/example/orient_me/schedules/ScheduleDatabaseHelper.java
@@ -1,5 +1,13 @@
 package com.example.orient_me.schedules;
 
+import android.content.Context;
+
+import com.example.orient_me.helpers.DatabaseHelper;
+
 public class ScheduleDatabaseHelper extends DatabaseHelper {
+
+	public ScheduleDatabaseHelper(Context context) {
+		super(context);
+	}
 
 }
