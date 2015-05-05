443a78000fc08ed515ddbee85aeabfc10dcf2dc0
Breena Theseira <breenatheseira@yahoo.com>
2015-05-05 18:42:13 +0800

    Refactored: Renamed title to names for ViewContactsListActivity

--- src/com/example/orient_me/contacts/ViewContactsListActivity.java
+++ src/com/example/orient_me/contacts/ViewContactsListActivity.java
@@ -1,21 +1,17 @@
-package contacts;
+package com.example.orient_me.contacts;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.orient_me.R;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
+
+import com.example.orient_me.R;
 
 public class ViewContactsListActivity extends ActionBarActivity {
 	
@@ -37,17 +33,18 @@ public class ViewContactsListActivity extends ActionBarActivity {
 
 	private void loadListView() {
 		ContactsDatabaseHelper db = new ContactsDatabaseHelper(this);
-		
-		ArrayList<String> titles = new ArrayList<String>(); 
-		
+
+		ArrayList<String> names = new ArrayList<String>(); 
+
 		//Tamada, R. (2013) Android SQLite Database with Multiple Tables. [Online]. Available from: http://www.androidhive.info/2013/09/android-sqlite-database-with-multiple-tables/ [Accessed: 1 May 2015].
 		contacts = db.getAllContacts();
+
 		for (Contact eachContact : contacts){
-			titles.add(eachContact.getName());
+			names.add(eachContact.getName());
 		}
 		
 		// Developers (n.d.) Layouts. [Online]. Available from: http://developer.android.com/guide/topics/ui/declaring-layout.html#AdapterViews [Accessed: 1 May 2015]. 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,titles);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,names);
 		
 		contactList.setAdapter(adapter);
 		contactList.setEmptyView(emptyLayout);
