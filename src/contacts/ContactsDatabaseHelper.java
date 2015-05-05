fd7e348aedc6c81384c1829a54fb0cc4275693a4
Breena Theseira <breenatheseira@yahoo.com>
2015-05-05 14:21:13 +0800

    Added: Contacts Database Helper

---
+++ src/contacts/ContactsDatabaseHelper.java
@@ -0,0 +1,13 @@
+package contacts;
+
+import android.content.Context;
+
+import com.example.orient_me.helpers.DatabaseHelper;
+
+public class ContactsDatabaseHelper extends DatabaseHelper {
+
+	public ContactsDatabaseHelper(Context context) {
+		super(context);
+	}
+
+}
