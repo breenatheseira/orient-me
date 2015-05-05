027695ef3077cc215cebdea08a4c70989135fc0b
Breena Theseira <breenatheseira@yahoo.com>
2015-05-05 14:43:35 +0800

    Added: Contact class to Contacts package.

---
+++ src/contacts/Contact.java
@@ -0,0 +1,38 @@
+package contacts;
+
+public class Contact {
+	String id, name, number, imported;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getImported() {
+		return imported;
+	}
+
+	public void setImported(String imported) {
+		this.imported = imported;
+	}
+	
+}
