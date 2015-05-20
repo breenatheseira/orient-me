0715bf93e253c2a4978b97b2a74d4676a2110f63
Breena Theseira <breenatheseira@yahoo.com>
2015-05-20 10:19:21 +0800

    Added: Geocoder class from Gupte to accurately obtain Lat and Lng from an address before passing it to Google Maps

--- src/com/example/orient_me/maps/MapActivity.java
+++ src/com/example/orient_me/maps/MapActivity.java
@@ -1,14 +1,15 @@
-package orient_me.maps;
+package com.example.orient_me.maps;
 
+import java.util.List;
+
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.orient_me.R;
-import com.example.orient_me.R.id;
-import com.example.orient_me.R.layout;
-import com.example.orient_me.R.menu;
+import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -29,7 +30,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     public void onMapReady(GoogleMap map) {
-        LatLng APU = new LatLng(3.0497641, 101.6959657);
+        LatLng APU = getLocationFromAddress("Asia Pacific University of Technology & Innovation (APU)");
 
         map.setMyLocationEnabled(true);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(APU, 17));
@@ -57,5 +58,29 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	// Gupte, N. A. (2015) How Can I Find the Latitude and Longitude From Address? [Online]. Available from: http://stackoverflow.com/questions/3574644/how-can-i-find-the-latitude-and-longitude-from-address/27834110#27834110 [Accessed: 20 May 2015].
+	public LatLng getLocationFromAddress(String strAddress) {
+
+	    Geocoder coder = new Geocoder(this);
+	    List<android.location.Address> address;
+	    LatLng p1 = null;
+
+	    try {
+	        address = coder.getFromLocationName(strAddress, 5);
+	        if (address == null) {
+	            return null;
+	        }
+	        android.location.Address location = address.get(0);
+	        location.getLatitude();
+	        location.getLongitude();
+
+	        p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	    return p1;
 	}
 }
