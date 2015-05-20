638c3b92814726b754049f833dc81bf198de0f94
Breena Theseira <breenatheseira@yahoo.com>
2015-05-20 15:28:14 +0800

    Refactored: Changed ActionBarActivity to AppCompatActivity as Action Bar is depreceated

--- src/com/example/orient_me/badges/ViewBadgesListActivity.java
+++ src/com/example/orient_me/badges/ViewBadgesListActivity.java
@@ -1,16 +1,13 @@
 package com.example.orient_me.badges;
 
-import com.example.orient_me.R;
-import com.example.orient_me.R.id;
-import com.example.orient_me.R.layout;
-import com.example.orient_me.R.menu;
-
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ViewBadgesListActivity extends ActionBarActivity {
+import com.example.orient_me.R;
+
+public class ViewBadgesListActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +17,12 @@ public class ViewBadgesListActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.view_badges_list, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
