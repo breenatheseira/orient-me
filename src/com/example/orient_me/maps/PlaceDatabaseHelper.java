baa6237047451cdb64624d8e192e4c52718b86bb
Breena Theseira <breenatheseira@yahoo.com>
2015-05-20 11:28:00 +0800

    Added: PlaceDatabaseHelper class to obtain the entire list of records for places

---
+++ src/com/example/orient_me/maps/PlaceDatabaseHelper.java
@@ -0,0 +1,39 @@
+package com.example.orient_me.maps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+
+import com.example.orient_me.helpers.DatabaseHelper;
+
+public class PlaceDatabaseHelper extends DatabaseHelper {
+
+	public PlaceDatabaseHelper(Context context) {
+		super(context);
+	}
+	
+	public List<Place> getAllPlaces() {
+		List<Place> places = new ArrayList<Place>();
+		String sql = "SELECT * FROM " + TABLE_PLACES;
+
+		Cursor c = rdb.rawQuery(sql, null);
+
+		if (c.moveToFirst()) {
+			do {
+				Place place = new Place();
+				place.setId(c.getString(c.getColumnIndex(PLACE_ID )));
+				place.setTitle(c.getString(c.getColumnIndex(PLACE_TITLE)));
+				place.setSnippet(c.getString(c.getColumnIndex(PLACE_SNIPPET)));
+				place.setAddress(c.getString(c.getColumnIndex(PLACE_ADDRESS)));
+				place.setLat(c.getString(c.getColumnIndex(PLACE_LAT)));
+				place.setLng(c.getString(c.getColumnIndex(PLACE_LNG)));
+				place.setType(c.getString(c.getColumnIndex(PLACE_TYPE)));
+
+				places.add(place);
+			} while (c.moveToNext());
+		}
+		return places;
+	}
+}
