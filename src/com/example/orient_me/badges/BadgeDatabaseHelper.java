86cf3b292e891989969a8c8a58c358d18c8dc448
Breena Theseira <breenatheseira@yahoo.com>
2015-05-20 15:01:20 +0800

    Updated: codes to obtain all Badges from database, refactored setting values for each Badge object's attributes to a separate function

--- src/com/example/orient_me/badges/BadgeDatabaseHelper.java
+++ src/com/example/orient_me/badges/BadgeDatabaseHelper.java
@@ -1,5 +1,8 @@
 package com.example.orient_me.badges;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -13,6 +16,18 @@ public class BadgeDatabaseHelper extends DatabaseHelper {
 		super(context);
 	}
 
+	private Badge setBadgesFromCursor(Cursor c){
+		Badge badge = new Badge();
+		
+		badge.setId(c.getString(c.getColumnIndex(BADGE_ID)));
+		badge.setName(c.getString(c.getColumnIndex(BADGE_NAME)));
+		badge.setDesc(c.getString(c.getColumnIndex(BADGE_DESC)));
+		badge.setPicture(c.getString(c.getColumnIndex(BADGE_PICTURE)));
+		badge.setUnlocked_at(c.getString(c.getColumnIndex(BADGE_UNLOCKED_AT)));
+		
+		return badge;
+	}
+	
 	public Badge getOneBadgeRow (String id) {
 		String sql = "Select * FROM " + TABLE_BADGES + " WHERE id = "
 				+ id;
@@ -20,14 +35,31 @@ public class BadgeDatabaseHelper extends DatabaseHelper {
 		Cursor c = rdb.rawQuery(sql, null);
 
 		if (c.moveToNext()) {
-			badge.setId(id);
-			badge.setName(c.getString(c.getColumnIndex(BADGE_NAME)));
-			badge.setDesc(c.getString(c.getColumnIndex(BADGE_DESC)));
-			badge.setPicture(c.getString(c.getColumnIndex(BADGE_PICTURE)));
-			badge.setUnlocked_at(c.getString(c.getColumnIndex(BADGE_UNLOCKED_AT)));
+			
 		}
 		return badge;
 	}
+	
+	public List<Badge> getAllBadges(){
+		List<Badge> badges = new ArrayList<Badge>();
+		String sql = "SELECT * FROM " + TABLE_BADGES;
+		
+		try {
+			Cursor c = rdb.rawQuery(sql, null);
+	
+			if (c.moveToFirst()) {
+				do {
+					badges.add(setBadgesFromCursor(c));
+				} while (c.moveToNext());
+			}
+		} catch (Exception e) {
+			badges = null;
+			Log.d(this.getClass().getSimpleName(), "Error returning all Badges: " + e.getMessage());
+		}
+		
+		return badges;
+	}
+	
 	public int updateBadge(Badge badge){
 		ContentValues values = new ContentValues();
 		values.put(BADGE_UNLOCKED_AT, badge.getUnlocked_at());
