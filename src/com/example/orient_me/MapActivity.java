2eebaa7949c85f1af266db26c92e62d0be7b27af
Breena Theseira <breenatheseira@yahoo.com>
2015-05-20 08:49:09 +0800

    Modified: Map code from Google tutorials to display the maps

--- src/com/example/orient_me/MapActivity.java
+++ src/com/example/orient_me/MapActivity.java
@@ -1,17 +1,17 @@
 package com.example.orient_me;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MapActivity extends ActionBarActivity {
+public class MapActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
