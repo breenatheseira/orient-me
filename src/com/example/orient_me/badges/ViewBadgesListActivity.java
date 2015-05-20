15ee8f15f56ffc6fef06d680c478ec1db10fc41b
Breena Theseira <breenatheseira@yahoo.com>
2015-05-20 16:16:38 +0800

    Added: Code to view the list of badges

--- src/com/example/orient_me/badges/ViewBadgesListActivity.java
+++ src/com/example/orient_me/badges/ViewBadgesListActivity.java
@@ -1,18 +1,53 @@
 package com.example.orient_me.badges;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.example.orient_me.R;
 
 public class ViewBadgesListActivity extends AppCompatActivity {
 
+	ListView badgesList;
+	LinearLayout emptyLayout;
+	List<Badge> badges;
+	BadgeDatabaseHelper db;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_badges_list);
+		
+		badgesList = (ListView) findViewById(R.id.avblLV_badges_list);
+		emptyLayout = (LinearLayout) findViewById(R.id.avblLL_list_layout);
+
+		loadListView();
+	}
+	
+	private void loadListView() {
+		db = new BadgeDatabaseHelper(this);
+
+		ArrayList<String> names = new ArrayList<String>(); 
+
+		//Tamada, R. (2013) Android SQLite Database with Multiple Tables. [Online]. Available from: http://www.androidhive.info/2013/09/android-sqlite-database-with-multiple-tables/ [Accessed: 1 May 2015].
+		badges = db.getAllBadges();
+
+		for (Badge eachBadge : badges){
+			names.add(eachBadge.getName());
+		}
+		
+		// Developers (n.d.) Layouts. [Online]. Available from: http://developer.android.com/guide/topics/ui/declaring-layout.html#AdapterViews [Accessed: 1 May 2015]. 		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,names);
+		
+		badgesList.setAdapter(adapter);
+		badgesList.setEmptyView(emptyLayout);
 	}
 
 	@Override
