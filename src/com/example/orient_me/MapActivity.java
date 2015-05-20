ef564fb0049070924e0df6a58e1d40dd6ffd2e84
Breena Theseira <breenatheseira@yahoo.com>
2015-05-20 10:01:59 +0800

    Updated: Displays APU markers by adding Latitude and Longitude

--- src/com/example/orient_me/MapActivity.java
+++ src/com/example/orient_me/MapActivity.java
@@ -5,14 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    public void onMapReady(GoogleMap map) {
+        LatLng APU = new LatLng(3.0497641, 101.6959657);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(APU, 17));
+
+        map.addMarker(new MarkerOptions()
+                .title("APU TPM Campus")
+                .snippet("Main Campus of APU in Malaysia")
+                .position(APU));
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
