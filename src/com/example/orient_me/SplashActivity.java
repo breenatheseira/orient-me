1c1254173684dd0fc6758e2fc09fc2f0282d4299
Breena Theseira <breenatheseira@yahoo.com>
2015-05-22 14:15:58 +0800

    Updated UI for Splash screen

--- src/com/example/orient_me/SplashActivity.java
+++ src/com/example/orient_me/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.example.orient_me;
 
 import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.example.orient_me.helpers.PreferencesHelper;
 
@@ -11,12 +15,28 @@ import com.example.orient_me.helpers.PreferencesHelper;
 public class SplashActivity extends AppCompatActivity {
 	private static int splash_timeout = 3000;
 	
+	ImageView image;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		 
+		image = (ImageView) findViewById(R.id.saIV_background);
 		
+		// Get actual image width and height from res/drawable
+		// BJK (2012) Android Get Width And Height Of Image From Resource. [Online]. Available from: http://spacetech.dk/android-get-width-and-height-of-image-from-resource.html [Accessed: 22 May 2015].
+		BitmapDrawable bd = (BitmapDrawable) this.getResources().getDrawable(R.drawable.apu_logo_final);
+		int imageHeight = bd.getBitmap().getHeight();
+		int imageWidth = bd.getBitmap().getWidth();
+		
+		// Erdfelt, J. (2011) Android Image View Matrix Scale + Translate. [Online]. Available from: http://stackoverflow.com/questions/6075363/android-image-view-matrix-scale-translate [Accessed: 22 May 2015].
+		// Matrix for proportional scaling
+		Matrix m = image.getImageMatrix();
+		RectF drawableRect = new RectF(0, 0, imageWidth, imageHeight);
+		RectF viewRect = new RectF(0, 0, image.getWidth(), image.getHeight());
+		m.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
+		image.setImageMatrix(m);
 		
 		new Handler().postDelayed(new Runnable() {
 			@Override
