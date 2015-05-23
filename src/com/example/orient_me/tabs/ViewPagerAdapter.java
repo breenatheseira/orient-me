d6863b8db068a077cc83ba9459172f4f56e7e8e6
Breena Theseira <breenatheseira@yahoo.com>
2015-05-23 21:28:01 +0800

    Added: Main fragments to View Page Adapter classes

--- src/com/example/orient_me/tabs/ViewPagerAdapter.java
+++ src/com/example/orient_me/tabs/ViewPagerAdapter.java
@@ -2,46 +2,59 @@
 
 package com.example.orient_me.tabs;
 
-import com.example.orient_me.DocumentFragment;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import com.example.orient_me.DocumentFragment;
+import com.example.orient_me.schedules.ViewScheduleListFragment;
+
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 	 
-    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
-    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+    CharSequence Titles[];
+    int tabAmount;
  
-    // Build a Constructor and assign the passed Values to appropriate values in the class
-    public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
+    public ViewPagerAdapter(FragmentManager fm, CharSequence mTitles[], int mNumbOfTabsumb) {
         super(fm);
  
         this.Titles = mTitles;
-        this.NumbOfTabs = mNumbOfTabsumb;
+        this.tabAmount = mNumbOfTabsumb;
     }
  
-    //This method return the fragment for the every position in the View Pager
     @Override
     public Fragment getItem(int position) {
     	Fragment item = new Fragment();
-        if(position == 0) // if the position is 0 we are returning the First tab
-        {
-            item = new DocumentFragment();
-        }
-        
+    	
+    	switch(position){
+    	case 0:
+    		item = new DocumentFragment();
+    		break;
+    	case 1:
+    		item = new ViewScheduleListFragment();
+    		break;
+    	case 2:
+    		item = new ViewContactsListFragment();
+    		break;
+    	case 3:
+    		item = new MapFragment();
+    		break;
+    	case 4:
+    		item = new FacebookFragment();
+    		break;
+    	case 5:
+    		item = new ViewBadgesListFragment();
+    		break;    		
+    	}
         return item;
     }
  
-    // This method return the titles for the Tabs in the Tab Strip
     @Override
     public CharSequence getPageTitle(int position) {
         return Titles[position];
     }
  
-    // This method return the Number of tabs for the tabs Strip
     @Override
     public int getCount() {
-        return NumbOfTabs;
+        return tabAmount;
     }
 }
