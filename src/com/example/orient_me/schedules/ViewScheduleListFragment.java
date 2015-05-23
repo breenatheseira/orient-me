87fe0a393919233ddfd50d461222d200b21eb286
Breena Theseira <breenatheseira@yahoo.com>
2015-05-23 21:26:14 +0800

    Changed: View Schedule List to a Fragment class

--- src/com/example/orient_me/schedules/ViewScheduleListFragment.java
+++ src/com/example/orient_me/schedules/ViewScheduleListFragment.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -24,19 +23,20 @@ import com.example.orient_me.R;
 import com.example.orient_me.badges.Badge;
 import com.example.orient_me.badges.BadgeDatabaseHelper;
 
-public class ViewScheduleListFragment extends AppCompatActivity {
+public class ViewScheduleListFragment extends Fragment {
 
 	ListView scheduleList;
 	LinearLayout emptyLayout;
 	List<Schedule> schedules;
-	
+	FragmentActivity context;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_view_schedule_list);
-		
-		scheduleList = (ListView) findViewById(R.id.vnsLV_listview);
-		emptyLayout = (LinearLayout) findViewById(R.id.vnslLL_emptyList);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	     context = (FragmentActivity) super.getActivity();
+
+	    LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.activity_view_schedule_list, container, false);
+	    
+		scheduleList = (ListView) layout.findViewById(R.id.vnsLV_listview);
+		emptyLayout = (LinearLayout) layout.findViewById(R.id.vnslLL_emptyList);
 		
 		loadListView();
 		scheduleList.setOnItemClickListener(new OnItemClickListener() {
@@ -47,18 +47,20 @@ public class ViewScheduleListFragment extends AppCompatActivity {
 				
 				String id = schedules.get(position).getId(); 
 				
-				Intent intent = new Intent(ViewScheduleListFragment.this, ViewScheduleActivity.class);
+				Intent intent = new Intent(context, ViewScheduleActivity.class);
 				intent.putExtra("id", id);
 				startActivity(intent);
 			}
 		});
+	    
+	    return layout ; 
 	}
 	
 	private void loadListView(){
-		ScheduleDatabaseHelper db = new ScheduleDatabaseHelper(ViewScheduleListFragment.this);
+		ScheduleDatabaseHelper db = new ScheduleDatabaseHelper(context);
 		
 		if (db.getAllSchedules().isEmpty()){
-			Toast.makeText(this, "Add a Schedule to View Your List", Toast.LENGTH_LONG).show();
+			Toast.makeText(context, "Add a Schedule to View Your List", Toast.LENGTH_LONG).show();
 			return;
 		}
 		
@@ -66,37 +68,14 @@ public class ViewScheduleListFragment extends AppCompatActivity {
 		if (schedules.size() == 5)
 			showAchievement(5);
 		
-		ScheduleListAdapter adapter = new ScheduleListAdapter(this, R.layout.custom_schedules_list, schedules);
+		ScheduleListAdapter adapter = new ScheduleListAdapter(context, R.layout.custom_schedules_list, schedules);
 		scheduleList.setAdapter(adapter);
 		scheduleList.setEmptyView(emptyLayout);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.add_only, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if (id == R.id.add) {
-			Intent intent = new Intent(this, AddScheduleActivity.class);
-			startActivity(intent);
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	protected void onPostResume() {
-		loadListView();
-		super.onPostResume();
-	}
-	
-	
     private void showAchievement(int id) {
 
-		BadgeDatabaseHelper db = new BadgeDatabaseHelper(this);
+		BadgeDatabaseHelper db = new BadgeDatabaseHelper(context);
 
 		Badge badge = db.getOneBadgeRow(String.valueOf(id));
 
@@ -105,17 +84,16 @@ public class ViewScheduleListFragment extends AppCompatActivity {
 			Log.d("VScheduleListA - Checking time format", badge.getUnlocked_at());
 			db.updateBadge(badge);
 			
-			LayoutInflater inflater = getLayoutInflater();
+			LayoutInflater inflater = context.getLayoutInflater();
 			View layout = inflater.inflate(R.layout.customtoast,
-					(ViewGroup) findViewById(R.id.toast_container));
+					(ViewGroup) context.findViewById(R.id.toast_container));
 
 			ImageView image = (ImageView) layout.findViewById(R.id.toast_image);
 			image.setImageResource(R.drawable.badge_5);
-			TextView badgeName = (TextView) layout
-					.findViewById(R.id.toast_text);
+			TextView badgeName = (TextView) layout.findViewById(R.id.toast_text);
 			badgeName.setText(badge.getName());
 
-			Toast toast = new Toast(getApplicationContext());
+			Toast toast = new Toast(context);
 			toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 100);
 			toast.setDuration(Toast.LENGTH_LONG);
 			toast.setView(layout);
