a8a3e50ca787b7600be7a890ec60f6aab17c5b04
Breena Theseira <breenatheseira@yahoo.com>
2015-05-23 14:29:11 +0800

    Updated: Set Import image to be clickable

--- src/com/example/orient_me/contacts/ContactsListAdapter.java
+++ src/com/example/orient_me/contacts/ContactsListAdapter.java
@@ -6,14 +6,16 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.orient_me.R;
 
-public class ContactsListAdapter extends ArrayAdapter<Contact>{
+public class ContactsListAdapter extends ArrayAdapter<Contact> implements OnClickListener {
 	
 	Context context;
 	List<Contact> contacts;
@@ -33,6 +35,7 @@ public class ContactsListAdapter extends ArrayAdapter<Contact>{
 		
 		TextView name = (TextView) contactListItem.findViewById(R.id.cclT_name);
 		ImageView imported = (ImageView) contactListItem.findViewById(R.id.cclIV_import);
+		imported.setOnClickListener(this);
 		
 		if ("1".equals(contacts.get(position).getImported()))
 			imported.setImageResource(R.drawable.ic_action_accept);
@@ -40,5 +43,10 @@ public class ContactsListAdapter extends ArrayAdapter<Contact>{
 		name.setText(contacts.get(position).getName());
 		
 		return contactListItem;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Toast.makeText(context, "hahaha", Toast.LENGTH_SHORT).show();
 	}
 }
