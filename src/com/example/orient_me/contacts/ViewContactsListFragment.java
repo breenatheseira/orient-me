0c4c66f51ad6ae9cef9e51474d0d471ca53690e1
Breena Theseira <breenatheseira@yahoo.com>
2015-05-26 23:32:53 +0800

    Updated: Instructions are visible on the Contacts page

--- src/com/example/orient_me/contacts/ViewContactsListFragment.java
+++ src/com/example/orient_me/contacts/ViewContactsListFragment.java
@@ -1,9 +1,32 @@
 package com.example.orient_me.contacts;
 
-public class ViewContactsListFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-	public ViewContactsListFragment() {
-		// TODO Auto-generated constructor stub
+import com.example.orient_me.R;
+
+public class ViewContactsListFragment extends Fragment {
+	FragmentActivity context;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	     context = (FragmentActivity) super.getActivity();
+
+	     LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.fragment_view_contact_list, container, false);
+	     TextView ins = (TextView) layout.findViewById(R.id.vclfT_instructions);
+	     
+	     ins.setText("Touch the download icons to save important APU contacts.");
+	     
+	     ContactsListFragment contactslist = (ContactsListFragment) getChildFragmentManager().findFragmentById(R.id.vclf_contactList);
+	     contactslist.getListView();
+	     
+	     return layout; 
 	}
 
 }
