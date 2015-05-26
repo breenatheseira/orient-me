6c86e8897ca54bd4cb27bf370f444b3698bb997c
Breena Theseira <breenatheseira@yahoo.com>
2015-05-26 22:39:35 +0800

    Renamed: ViewBadgesListFragment > BadgesListFragment

--- src/com/example/orient_me/badges/BadgesListFragment.java
+++ src/com/example/orient_me/badges/BadgesListFragment.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.ListFragment;
 
-public class ViewBadgesListFragment extends ListFragment {
+public class BadgesListFragment extends ListFragment {
 
 	List<Badge> badges;
 	BadgeDatabaseHelper db;
