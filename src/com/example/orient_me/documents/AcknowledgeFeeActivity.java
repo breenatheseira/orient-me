853f80f163fd8831a43924fcebf4d0d7d355d20b
Breena Theseira <breenatheseira@yahoo.com>
2015-06-04 16:25:19 +0800

    Updated: Fee Webviews have been extracted as a Fragment

--- src/com/example/orient_me/documents/AcknowledgeFeeActivity.java
+++ src/com/example/orient_me/documents/AcknowledgeFeeActivity.java
@@ -1,35 +1,29 @@
 package com.example.orient_me.documents;
 
 import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import com.example.orient_me.R;
-import com.example.orient_me.helpers.PreferencesHelper;
 
 public class AcknowledgeFeeActivity extends AppCompatActivity {
-
+	
+	Button acknowledgeButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_acknowledge_fee);
+		acknowledgeButton = (Button) findViewById(R.id.aafB_acknowledgeButton);
+		FragmentManager fragmentManager = getFragmentManager(); 
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 		
-//		WebView layout = (WebView) findViewById(R.id.aafWV_fee_schedule);
-		WebView w = new WebView(this);
-	    
-	    w = (WebView) findViewById(R.id.aafWV_fee_schedule); 
-	    
-	    //Cristian (2010) What's the Difference Between setWebViewClient vs setWebChromeClient? [Online]. Available from: http://stackoverflow.com/questions/2835556/whats-the-difference-between-setwebviewclient-vs-setwebchromeclient [Accessed: 19 May 2015].
-	    w.setWebChromeClient(new WebChromeClient()); 
-	    w.setWebViewClient(new WebViewClient()); 
-	    w.getSettings().setJavaScriptEnabled(true);
-		
-	    PreferencesHelper prefs = new PreferencesHelper(this); 
-	     
-	    String url = prefs.GetPreferences("CourseScheduleURL");
-	    w.loadUrl(url);
+		Fragment fragment = new Fragment();
+		fragmentTransaction.add(R.id.aaf_feeFragment, fragment); 
+		fragmentTransaction.commit();
 	}
 
 }
---
+++ src/com/example/orient_me/documents/FeeFragment.java
@@ -0,0 +1,46 @@
+package com.example.orient_me.documents;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.orient_me.R;
+import com.example.orient_me.helpers.PreferencesHelper;
+
+public class FeeFragment extends Fragment {
+	
+	FragmentActivity context;
+	
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	     context = (FragmentActivity) super.getActivity();
+
+	     WebView layout = (WebView) inflater.inflate(R.layout.fragment_fee, container, false);
+	     WebView w = new WebView(context);
+	    
+	     w = (WebView) layout.findViewById(R.id.feeWebView); 
+	     
+	     //Cristian (2010) What's the Difference Between setWebViewClient vs setWebChromeClient? [Online]. Available from: http://stackoverflow.com/questions/2835556/whats-the-difference-between-setwebviewclient-vs-setwebchromeclient [Accessed: 19 May 2015].
+	     w.setWebChromeClient(new WebChromeClient()); 
+	     w.setWebViewClient(new WebViewClient()); 
+		
+	     PreferencesHelper prefs = new PreferencesHelper(context); 
+	     
+	     String url = "http://docs.google.com/gview?embedded=true&url=" + prefs.GetPreferences("FeeScheduleURL");	     
+	     Log.d("FeeFragment", url);
+	     // Javascript must be enabled in order to display the PDF.
+	     w.getSettings().setJavaScriptEnabled(true);
+	     w.loadUrl(url);
+	     return layout; 
+	}
+
+}
