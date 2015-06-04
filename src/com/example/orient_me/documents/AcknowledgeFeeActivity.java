c4992b8ccaca692b3b054c0b2e09981129b09b35
Breena Theseira <breenatheseira@yahoo.com>
2015-06-04 16:48:52 +0800

    Updated: Styling, button is at the bottom, toolbar has a title, and proper layouts were used

--- src/com/example/orient_me/documents/AcknowledgeFeeActivity.java
+++ src/com/example/orient_me/documents/AcknowledgeFeeActivity.java
@@ -5,25 +5,32 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 
 import com.example.orient_me.R;
 
 public class AcknowledgeFeeActivity extends AppCompatActivity {
-	
+
 	Button acknowledgeButton;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_acknowledge_fee);
-		acknowledgeButton = (Button) findViewById(R.id.aafB_acknowledgeButton);
-		FragmentManager fragmentManager = getFragmentManager(); 
-		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-		
+
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager
+				.beginTransaction();
+
 		Fragment fragment = new Fragment();
-		fragmentTransaction.add(R.id.aaf_feeFragment, fragment); 
+		fragmentTransaction.add(R.id.aaf_feeFragment, fragment);
 		fragmentTransaction.commit();
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		acknowledgeButton = (Button) findViewById(R.id.aafB_acknowledgeButton);
 	}
 
 }
