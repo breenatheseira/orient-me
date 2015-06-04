dc52d716ba94b7e628cfd9ef6b5a8982fd4857a2
Breena Theseira <breenatheseira@yahoo.com>
2015-06-04 17:38:00 +0800

    Updated: Acknowledge button is clickable

--- src/com/example/orient_me/documents/AcknowledgeFeeActivity.java
+++ src/com/example/orient_me/documents/AcknowledgeFeeActivity.java
@@ -1,16 +1,21 @@
 package com.example.orient_me.documents;
 
 import android.os.Bundle;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.orient_me.R;
 
-public class AcknowledgeFeeActivity extends AppCompatActivity {
+public class AcknowledgeFeeActivity extends AppCompatActivity implements OnClickListener {
 
 	Button acknowledgeButton;
 
@@ -31,6 +36,23 @@ public class AcknowledgeFeeActivity extends AppCompatActivity {
 		setSupportActionBar(toolbar);
 
 		acknowledgeButton = (Button) findViewById(R.id.aafB_acknowledgeButton);
+		acknowledgeButton.setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View v) {
+		new AlertDialog.Builder(this)
+	      .setMessage("Are you sure you acknowledge the fee statement?")
+	      .setCancelable(false)
+	      .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+	          public void onClick(DialogInterface dialog, int id) {
+	        	  makeToastMsg();
+	          }
+	      })
+	      .setNegativeButton("No", null)
+	      .show();
+	}
+	public void makeToastMsg(){
+		Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
+	}
 }
