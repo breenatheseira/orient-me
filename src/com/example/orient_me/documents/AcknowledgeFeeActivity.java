27407da0e219b7fca8484e533e942b055af9e090
Breena Theseira <breenatheseira@yahoo.com>
2015-06-04 11:59:16 +0800

    Added: Acknowledge Fee Activity + designed the layout + Acknowledge Fee Activity is in the Documents Folder

---
+++ src/com/example/orient_me/documents/AcknowledgeFeeActivity.java
@@ -0,0 +1,17 @@
+package com.example.orient_me.documents;
+
+import com.example.orient_me.R;
+import com.example.orient_me.R.layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AcknowledgeFeeActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_acknowledge_fee);
+	}
+
+}
